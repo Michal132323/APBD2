9e7b099949f9bf3b29e2318c5ccd821e5e7467b2
Michał Mosiądz <s27923@pjwstk.edu.pl>
2024-03-12 13:33:07 +0100

    zad 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+int[] integers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+Console.WriteLine(GetAvg(integers));
+static double GetAvg(int[] ints)
+{
+    double sum = 0;
+    for (int i = 0; i < ints.Length; i++)
+    {
+        sum = (double)ints[i] + sum;
+    }
+
+    return sum / ints.Length;
+}
